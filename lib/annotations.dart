c7316a42c88511b56120da0442d4d32f2877d880
Sameera Piyadigamage <sameera.piyadigamage@carv.com>
2023-02-22 10:58:47 +0530

    change main export file name

--- lib/annotations.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, the Carv App. All rights reserved.
-
-/// Provides annotation classes to use with
-/// [carv_generators](https://pub.dev/packages/carv_generators).
-///
-library carv_annotations;
-
-export 'src/validator_annotation.dart';
